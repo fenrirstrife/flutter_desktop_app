24a0fb94471037eaf96d1ab685c6da21068162f1
Jesus Cerecer <jesus.cerecer@hotmail.com>
2022-01-06 00:46:42 -0600

    base code for flutter desktop codelab

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '0cfd242baffc5e27d139';
+const githubClientSecret = 'cd7f6ec135a3bc3ae3b6b2bb48fcfa389f4d9d25';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
