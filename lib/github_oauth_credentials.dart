a8e562918f339f824b229a2f2ac786532b26ca03
Jesus A. R. Cerecer <jesus.cerecer@hotmail.com>
2022-01-06 00:47:27 -0600

    Update github_oauth_credentials.dart
    removed credentials

--- lib/github_oauth_credentials.dart
+++ lib/github_oauth_credentials.dart
@@ -1,5 +1,5 @@
-const githubClientId = '0cfd242baffc5e27d139';
-const githubClientSecret = 'cd7f6ec135a3bc3ae3b6b2bb48fcfa389f4d9d25';
+const githubClientId = 'YOUR_CLIENT_ID';
+const githubClientSecret = 'YOUR_CLIENT_SECRET';
 
 // OAuth scopes for repository and user information
 const githubScopes = ['repo', 'read:org'];
